935481999d4e84c15c9a04a8abd6a406097cf18e
Lorena Cardoso <lollycardoso@eaportal.org>
2025-05-27 23:26:41 -0300

    final

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Header } from "./components/Header"
+import { Ex1 } from "./components/Ex1"
+import { Ex2 } from "./components/Ex2"
+
+export function App(){
+  return (
+      <>
+      <Header />
+
+      <main>
+        <Ex1 />
+        <Ex2 />
+      </main>
+      </>
+  )
+}
